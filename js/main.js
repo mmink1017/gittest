ebc6eacaa104010fdec924ea2e229156d798c94f
min <mmink021017@gmail.com>
2026-08-12 15:31:22 +0900

    ㅎ

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ $title.addEventListener("click", (e) => {
   alert("환영합니다");
 });
 
+//다크모드 적용하기
 const toggleBtn = document.querySelector("#theme-toggle");
 toggleBtn.addEventListener("click", (e) => {
   document.body.classList.toggle("dark");
 });
+
+const validation = function () {
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+};
