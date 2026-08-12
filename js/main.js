c75c3516bc1cc0f0014517059a8392b035a5957c
min <mmink021017@gmail.com>
2026-08-12 14:28:14 +0900

    feat(main) :main.js에 버튼 이벤트 로직 추가

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ const $title = document.querySelector("h1");
 $title.addEventListener("click", (e) => {
   alert("환영합니다");
 });
+
+const toggleBtn = document.querySelector("#theme-toggle");
+toggleBtn.addEventListener("click", (e) => {
+  document.body.classList.toggle("dark");
+});
